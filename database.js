eefd1cb1063464a8627b73c05457400233abc8e7
ViniciusGalassi <110936788+ViniciusGalassi@users.noreply.github.com>
2022-09-22 22:25:29 -0300

    +Disciplinas e coleção de professores

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 db = db.getSiblingDB("puccamp");
 
+///Coleção de alunos
+
 db.alunos.insert(
 {    
-     Ra: 22003967 , 
+     ra: 22003967 , 
      primeiro_nome: "Gabriel",
      sobrenome: "Yamamoto", sexo: "Masculino", 
      disciplinas: ["Calculo I", "PI: Projetos de Desenvolvimento Web","Organização de Sistemas de Computação","Teologia e Fenomeno Humano", "Robotica Computacional", "Robotica Computacional (Pratica)","Organização de Sistemas de Computação (Pratica)","Calculo I (Pratica)", "PI: Projetos de Desenvolvimento Web (Pratica)"]
@@ -10,7 +12,7 @@ db.alunos.insert(
 
 db.alunos.insert(
 {
-    Ra: 22005252 , 
+    ra: 22005252 , 
     primeiro_nome: "Arthur", 
     sobrenome: "Maluf", 
     sexo: "Masculino", 
@@ -19,7 +21,7 @@ disciplinas: ["Calculo I", "PI: Projetos de Desenvolvimento Web","Organização 
 
 db.alunos.insert(
 { 
-    Ra: 22005768 , 
+    ra: 22005768 , 
     primeiro_nome: "Vinicius", 
     sobrenome: "Galassi", 
     sexo: "Masculino",
@@ -27,33 +29,140 @@ db.alunos.insert(
 
 db.alunos.insert(
 { 
-    Ra: 22002241 ,
+    ra: 22002241 ,
     primeiro_nome: "Joao", 
     sobrenome: "Pamponet", 
     sexo: "Masculino", 
     disciplinas: ["Calculo I", "PI: Projetos de Desenvolvimento Web","Organização de Sistemas de Computação","Teologia e Fenomeno Humano", "Robotica Computacional", "Robotica Computacional (Pratica)","Organização de Sistemas de Computação (Pratica)","Callo I (Pratica)", "PI: Projetos de Desenvolvimento Web (Pratica)"]
 })
 
+///Coleção de Disciplinas
+
 db.diciplinas.insert(
 {
-    Id_Dic: "12902", 
+    id_Dic: "12902", 
     nome: "PI: Projetos de Desenvolvimento Web",
-    Professores: ["Leandro Xastre"], 
+    professores: ["Leandro Xastre"], 
     qtd_cred: 4})
 
-db.diciplinas.insert({Id_Dic: "12902P",
-     nome: "PI: Projetos de Desenvolvimento Web (Pratica)",
-     Professores: ["Leandro Xastre"], qtd_cred: 2})
+db.diciplinas.insert({
+    id_Dic: "12902P",
+    nome: "PI: Projetos de Desenvolvimento Web (Pratica)",
+    professores: ["Leandro Xastre"],
+    qtd_cred: 2})
 
 db.diciplinas.insert({
-    Id_Dic: "12902", 
+    id_Dic: "12902", 
     nome: "Calculo I",
-    Professores: ["Valdomiro Santos"], 
+    professores: ["Valdomiro Santos"], 
     qtd_cred: 4 })
 
 db.diciplinas.insert(
 {
-    Id_Dic: "12902", 
+    id_Dic: "12902", 
     nome: "Calculo I (Pratica) ",
-    Professores: ["Alexandre Da Silva","Leandro Aguiar"],
+    professores: ["Alexandre Da Silva","Leandro Aguiar"],
     qtd_cred: 2 })
+
+db.diciplinas.insert(
+{
+    id_Dic: "12901", 
+    nome: "Robotica Computacional ",
+    professores: ["Dimas Lemes","Vinicius Simionatto"],
+    qtd_cred: 4 })
+
+db.diciplinas.insert(
+{
+    id_Dic: "12901P", 
+    nome: "Robotica Computacional (Pratica) ",
+    professores: ["Sergio Marques","Vinicius Simionatto"],
+    qtd_cred: 2 })
+
+db.diciplinas.insert(
+{
+    id_Dic: "12901P", 
+    nome: "Organizacao de Sistemas de Computacao ",
+    professores: ["Ricardo Pannain"],
+    qtd_cred: 4 })
+
+db.diciplinas.insert(
+{
+    id_Dic: "12903P", 
+    nome: "Organizacao de Sistemas de Computacao (Pratica)",
+    professores: ["Ricardo Pannain"],
+    qtd_cred: 2 })
+
+db.diciplinas.insert(
+{
+    id_Dic: "11135", 
+    nome: "Teologia e Fenomeno Humano",
+    professores: ["Danilo Rodrigues"],
+    qtd_cred: 2 })
+
+
+//Coleção de Professores
+
+db.professores.insert(
+{
+    rp: "1",
+    nome: "Ricardo Pannain",
+    disciplinas: ["Organizacao de Sistemas de Computacao", "Organizaco de Sistemas de Computacao (Pratica)"]
+
+})
+
+db.professores.insert(
+{
+    rp:"2",
+    nome: "Leandro Xastre",
+    disciplinas: ["PI: Desenvolvimentos de Sistemas Web","PI: Desenvolvimentos de Sistemas Web (Pratica)"]
+})
+
+db.professores.insert(
+{
+    rp:"3",
+    nome: "Valdomiro Santos",
+    disciplinas: ["Calculo I"]
+})
+
+db.professores.insert(
+{
+    rp:"4",
+    nome: "Alexandre Monteiro",
+    disciplinas: ["Calculo I (Pratica"]
+})
+
+db.professores.insert(
+{
+    rp:"5",
+    nome: "Leandro Aguiar",
+    disciplinas: ["Calculo I (Pratica)"]
+})
+
+db.professores.insert(
+{
+    rp:"6",
+    nome: "Dimas Lemes",
+    disciplinas: ["Robotica Computacional"]
+})
+
+db.professores.insert(
+{
+    rp: "7",
+    nome: "Vinicius Simionatto",
+    disciplinas: ["Robotica Computacional","Robotica Computacional (Pratica)"]
+})
+
+db.professores.insert(
+{
+    rp: "8",
+    nome: "Sergio Marques",
+    disciplinas: ["Robotica Computacional (Pratica)"]
+})
+
+db.professores.insert(
+{
+    rp:"9",
+    nome: "Danilo Rodrigues",
+    disciplinas: ["Teologia e fenomeno humano"]
+})
+    
